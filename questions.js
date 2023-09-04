b627a82be31d97a5e3071c985aa8086f1df8908b
LAVANYA <lavanyach313@gmail.com>
2023-09-04 13:07:31 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,118 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Languauge",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "The CSS property used to specify the transparency of an element is?",
+        answer: "A. opacity",
+        options: [
+            "A. opacity",
+            "B. visibility",
+            "C. filter",
+            "D. None of the above"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Which of the following is the correct syntax to make the background-color of all paragraph elements to yellow?",
+        answer: "B. p {background-color : yellow;}",
+        options: [
+            "A. p {background-color : #yellow;}",
+            "B. p {background-color : yellow;}",
+            "C. all {background-color : yellow;}",
+            "D. all p {background-color : #yellow;}"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "Which of the following is the correct syntax to display the hyperlinks without any underline?",
+        answer: "C. a {text-decoration : none;}",
+        options: [
+            "A. a {text-decoration : underline;}",
+            "B. a {decoration : no-underline;}",
+            "C. a {text-decoration : none;}",
+            "D. None of the above"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "The property in CSS used to change the text color of an element is -",
+        answer: "B. color",
+        options: [
+            "A. bgcolor",
+            "B. color",
+            "C. background-color",
+            "D. All of the above"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Who is the father of HTML",
+        answer: "C. Tim Berners-Lee",
+        options: [
+            "A. Rasmus Lerdorf",
+            "B. Brendan Eich",
+            "C. Tim Berners-Lee",
+            "D. Sergey Brin"
+        ]
+    },
+    {
+        numb: 7,
+        question: "In which part of the HTML metadata is contained?",
+        answer: "B. head tag",
+        options: [
+            "A. title tag",
+            "B. head tag",
+            "C. html tag",
+            "D. body tag"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Who is making the Web standards?",
+        answer: "D. The World Wide Web Consortium",
+        options: [
+            "A. Mozilla",
+            "B. Microsoft",
+            "C. Google",
+            "D. The World Wide Web Consortium"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The CSS property used to specify the transparency of an element is?",
+        answer: "A. opacity",
+        options: [
+            "A. opacity",
+            "B. visibility",
+            "C. filter",
+            "D. None of the above"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Which of the following is the correct syntax to make the background-color of all paragraph elements to yellow?",
+        answer: "B. p {background-color : yellow;}",
+        options: [
+            "A. p {background-color : #yellow;}",
+            "B. p {background-color : yellow;}",
+            "C. all {background-color : yellow;}",
+            "D. all p {background-color : #yellow;}"
+        ]
+    }
+    
+];
